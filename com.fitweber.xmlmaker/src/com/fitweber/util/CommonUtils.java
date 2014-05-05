b5633898ff916efa316e6e5815262019b5f514d4
wheat <hajima11@163.com>
2014-05-05 18:40:12 +0800

    更新工具类

--- com.fitweber.xmlmaker/src/com/fitweber/util/CommonUtils.java
+++ com.fitweber.xmlmaker/src/com/fitweber/util/CommonUtils.java
@@ -1,8 +1,12 @@
 package com.fitweber.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
